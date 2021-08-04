997d1dad403b385fcc542c687a4b1b561f392cc0
unknown <nhatdang081@gmail.com>
2021-08-04 08:28:44 -0700

    giai pt bac hai ax^2 + bx +c =0

--- exam64/bt64.cpp
+++ exam64/bt64.cpp
@@ -1,23 +1,24 @@
 #include<stdio.h>
 
 // giai pt ax+b = 0
-int main() {
-
-	printf(" nhap a va b : ") ;
-	int a, b ;
-	float x ;
-	scanf("%d%d", &a, &b );
-	if(a == 0) {
+void phuongTrinh(int a, int b) {
+		int x ;
+		if(a == 0) {
 		if(b == 0 ) {
 			printf("pt co vo so nghiem\n ") ;
 		} else
 			printf("pt vo nghiem\n ") ;
-
 	} else if(a != 0) {
 		x = (-b/a) ;
-
-		printf("pt co nghiem x = %f", x) ;
+		printf("pt co nghiem x = %d", x) ;
 	}
+}
+int main() {
 
+	printf(" nhap a va b : ") ;
+	int a, b ;
+	int x ;
+	scanf("%d%d", &a, &b );
+	phuongTrinh(a,b);
 	return 0 ;
 }
---
+++ exam65/bt65.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+//giai pt bac hai ax^2+bx+c=0
+void pt(int a, int b, int c) {
+	int  x, x1, x2, delta ;
+		delta =b*b - 4*a*c ;
+	if(delta < 0 ) {
+		printf("pt vo nghiem");
+	} else 
+	if(delta == 0 ) {
+		x = -b/2*a ; 
+		printf("pt co nghiem kep x = %d", x) ;
+	} else 
+	if(delta > 0) {
+		x1 = (- b + sqrt(delta)) / 2*a ;
+		x2 = (- b - sqrt(delta)) / 2*a ;
+		printf("pt co 2 nghiem x1 = %d\n x2 = %d\n", x1, x2) ;
+		
+	}
+}
+int main() {
+	
+	printf("nhap a, b, c :") ;
+	int a, b, c ;
+	scanf("%d%d%d", &a, &b, &c) ;
+	pt(a,b,c);
+	return 0 ;
+}
